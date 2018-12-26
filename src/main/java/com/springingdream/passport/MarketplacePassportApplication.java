5c02b27d2704cd7b4683d8d5cab4fdb0a6b9bab9
DeKinci <knd2000.2@gmail.com>
2018-12-26 07:23:44 +0300

    enabled eureka client

--- src/main/java/com/springingdream/passport/MarketplacePassportApplication.java
+++ src/main/java/com/springingdream/passport/MarketplacePassportApplication.java
@@ -2,8 +2,10 @@ package com.springingdream.passport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MarketplacePassportApplication {
 
     public static void main(String[] args) {
