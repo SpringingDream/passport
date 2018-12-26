230168f8f5be00e5e20a5a0fd8815a896e28b22b
DeKinci <knd2000.2@gmail.com>
2018-12-26 07:59:33 +0300

    did not helped

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-export DOCKER_HOST_IP=$(curl --retry 3 --connect-timeout 2 --max-time 2  -s 169.254.169.254/latest/meta-data/public-ipv4)
-
-echo $DOCKER_HOST_IP > /var/log/dockerIp.log
-
-exec "$@"
